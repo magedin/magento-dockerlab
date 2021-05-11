a592073b9eb191e26251e47ff1a177052901d593
Tiago Sampaio <tiago@tiagosampaio.com>
2021-05-11 18:17:21 -0300

    Removing unnecessary file.

--- queue.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source ~/.bashrc
-
-#bin/magento mimercado:orders_invoice:collect -o 5812
-#bin/magento mimercado:orders_invoice:collect
-bin/magento q:c:s mimercado.order.invoice.consumer
-
